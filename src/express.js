aca3f7b6dc4987f673b7833ba5b5a8dccb1cde2a
hansolpark <dundunki@naver.com>
2023-03-13 18:17:32 +0900

    add: body-parser

--- src/express.js
+++ src/express.js
@@ -1,12 +1,33 @@
+const exp = require('constants')
 const express = require('express')
+const http = require('http')
 
-const app = express()
-const port = 3001
+// const app = express()
+// const port = 3001
 
-app.get('/', (req, res) => {
-  res.send('express response test')
-})
+// app.get('/', (req, res) => {
+//   res.send('express response test')
+// })
 
-app.listen(port, () => {
-  console.log(`express server test port : ${port}`)
-})
+// app.listen(port, () => {
+//   console.log(`express server test port : ${port}`)
+// })
+
+class ApiServer extends http.Server {
+  constructor(config) {
+    const app = express()
+    super()
+    this.config = config
+    this.app = app
+    this.currentCons = new Set()
+    this.busy = new WeakSet()
+    this.stopping = false
+  }
+
+  async start() {}
+}
+
+const init = async (config = {}) => {
+  const server = new ApiServer(config)
+  return server.start()
+}
